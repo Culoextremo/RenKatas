eeb1d47785d51e30c8871893098c6ac064702bcc
cmolina <claramolinadev@gmail.com>
2023-11-12 12:47:02 +0100

    feat: throw card on top of same card

--- UnoRen/UnitTest1.cs
+++ UnoRen/UnitTest1.cs
@@ -1,3 +1,8 @@
+
+
+
+using FluentAssertions;
+
 namespace UnoRen;
 
 public class Tests
@@ -5,6 +10,25 @@ public class Tests
     [Test]
     public void METHOD()
     {
-        
+        var sut = new Card(Color.Yellow, 4);
+        sut.CanBeThrownOnTopOf(new Card(Color.Yellow, 4))
+            .Should().BeTrue();
     }
+}
+
+public class Card
+{
+    public Card(Color yellow, int i)
+    {
+    }
+
+    public bool CanBeThrownOnTopOf(Card card)
+    {
+        return true;
+    }
+}
+
+public enum Color
+{
+    Yellow
 }
