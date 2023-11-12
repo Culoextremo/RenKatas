6a135a29614a000ba7b078b6f77e5171728d0fde
cmolina <claramolinadev@gmail.com>
2023-11-12 13:21:44 +0100

    feat: check if player can throw card on table

--- UnoRen/UnitTest1.cs
+++ UnoRen/UnitTest1.cs
@@ -47,4 +47,38 @@ public class Tests
 
         sut.CardOnTop.Should().Be(new Card(Color.Yellow, 7));
     }
+    
+    [Test]
+    public void sdfsdf()
+    {
+        var sut = new Player(new Card(Color.Green, 3));
+        var doc = new Table(new Card(Color.Yellow, 7));
+        sut.CanThrowOn(doc)
+            .Should().Be(false);
+    }
+    
+    [Test]
+    public void dfgd()
+    {
+        var sut = new Player(new Card(Color.Green, 3));
+        var doc = new Table(new Card(Color.Yellow, 3));
+        sut.CanThrowOn(doc)
+            .Should().Be(true);
+    }
+    
+}
+
+public class Player
+{
+    private readonly Card[] hand;
+
+    public Player(params Card[] cards)
+    {
+        this.hand = cards;
+    }
+
+    public bool CanThrowOn(Table table)
+    {
+        return hand.Single().CanBeThrownOnTopOf(table.CardOnTop);
+    }
 }
