331d60bc1edf0e66797e9513c141672cef950a08
Ángel Siendones Sillero <culoextremo@gmail.com>
2023-11-12 12:54:33 +0100

    feat: prevent throw on card with different color and number

--- UnoRen/UnitTest1.cs
+++ UnoRen/UnitTest1.cs
@@ -14,21 +14,45 @@ public class Tests
         sut.CanBeThrownOnTopOf(new Card(Color.Yellow, 4))
             .Should().BeTrue();
     }
+
+    [Test]
+    public void aksdjksjkad()
+    {
+        var sut = new Card(Color.Yellow, 4);
+        sut.CanBeThrownOnTopOf(new Card(Color.Green, 4))
+            .Should().BeTrue();
+    }
+
+    [Test]
+    public void asdasdasd()
+    {
+        var sut = new Card(Color.Yellow, 8);
+        sut.CanBeThrownOnTopOf(new Card(Color.Green, 4))
+            .Should().BeFalse();
+    }
+    
+    //Otro numero mismo color
 }
 
 public class Card
 {
-    public Card(Color yellow, int i)
+    readonly Color color;
+    readonly int number;
+
+    public Card(Color color, int number)
     {
+        this.color = color;
+        this.number = number;
     }
 
-    public bool CanBeThrownOnTopOf(Card card)
+    public bool CanBeThrownOnTopOf(Card other)
     {
-        return true;
+        return other.color == this.color || other.number == this.number;
     }
 }
 
 public enum Color
 {
-    Yellow
+    Yellow,
+    Green
 }
