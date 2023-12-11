b2ef027209b8b45e89577dc690f9b5604f8343a9
cmolina <claramolinadev@gmail.com>
2023-12-11 21:41:55 +0100

    feat: view files

---
+++ VistaLoquesea/Program.cs
@@ -0,0 +1,13 @@
+﻿using UnoRen;
+using VistaLoquesea;
+
+var player1 = new Player(new Card(Color.Yellow, 5), new Card(Color.Yellow, 3));
+var player2 = new Player(new Card(Color.Yellow, 5));
+var game = new Game(player1, player2, new DrawPile(new Card(Color.Yellow, 5)), new DiscardPile(new Card(Color.Yellow, 5)));
+
+var doc = new ThrowCard(game);
+var sut = new Gameplay(game, doc,  new FakeInput(0.5f, new Card(Color.Yellow, 5)), new ConsoleGameView());
+
+await sut.Play();
+
+
