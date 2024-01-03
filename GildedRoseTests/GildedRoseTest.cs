a39be8c84a1b82967bad8f03c92ea969c7d07141
cmolina <claramolinadev@gmail.com>
2024-01-03 18:38:19 +0100

    test: decrease quality after sellin date

--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -9,7 +9,7 @@ namespace GildedRoseTests;
 public class GildedRoseTest
 {
     [Test]
-    public void akdjkajkds()
+    public void PassADay()
     {
         var items = new List<Item> { new Item { Name = "foo", SellIn = 10, Quality = 10 } };
         var sut = new GildedRose(items);
@@ -18,5 +18,29 @@ public class GildedRoseTest
 
         sut.Items.Single().SellIn.Should().Be(9);
         sut.Items.Single().Quality.Should().Be(9);
+    }
+    
+    [Test]
+    public void PsdfgsdfgdsgDay()
+    {
+        var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 20 } };
+        var sut = new GildedRose(items);
+        
+        sut.UpdateQuality();
+        
+        sut.Items.Single().SellIn.Should().Be(-1);
+        sut.Items.Single().Quality.Should().Be(18);
+    }
+    
+    [Test]
+    public void PsdfgsddfagsdfgfgdsgDay()
+    {
+        var items = new List<Item> { new Item { Name = "foo", SellIn = 1, Quality = 20 } };
+        var sut = new GildedRose(items);
+        
+        sut.UpdateQuality();
+        
+        sut.Items.Single().SellIn.Should().Be(0);
+        sut.Items.Single().Quality.Should().Be(19);
     }
 }
