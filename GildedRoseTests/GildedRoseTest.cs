5d468efdfa8f66c82867ea98f79a220f048b9e82
Ángel Siendones Sillero <culoextremo@gmail.com>
2024-01-03 18:30:36 +0100

    test: decrease quality

---
+++ GildedRoseTests/GildedRoseTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using GildedRoseKata;
+using NUnit.Framework;
+
+namespace GildedRoseTests;
+
+public class GildedRoseTest
+{
+    [Test]
+    public void akdjkajkds()
+    {
+        var items = new List<Item> { new Item { Name = "foo", SellIn = 10, Quality = 10 } };
+        var sut = new GildedRose(items);
+        
+        sut.UpdateQuality();
+
+        sut.Items.Single().SellIn.Should().Be(9);
+        sut.Items.Single().Quality.Should().Be(9);
+    }
+}
