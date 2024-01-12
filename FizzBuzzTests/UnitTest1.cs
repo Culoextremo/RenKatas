57fae6d7142c6bd47bd927884b37a2b1875e08ec
cmolina <claramolinadev@gmail.com>
2024-01-12 10:53:25 +0100

    refactor: toString method

--- FizzBuzzTests/UnitTest1.cs
+++ FizzBuzzTests/UnitTest1.cs
@@ -10,45 +10,51 @@ public class Tests
     public void dsfghfdsgh()
     {
         var sut = new Output(1);
-        sut.value.Should().Be("1");
+        sut.output.Should().Be("1");
     }
     
     [Test]
     public void shgfdghdfg()
     {
         var sut = new Output(2);
-        sut.value.Should().Be("2");
+        sut.output.Should().Be("2");
     }
     
     [Test]
     public void sdfghdfghd()
     {
         var sut = new Output(3);
-        sut.value.Should().Be("Fizz");
+        sut.output.Should().Be("Fizz");
     }
     
     [Test]
     public void hgjfghjfgh()
     {
         var sut = new Output(6);
-        sut.value.Should().Be("Fizz");
+        sut.output.Should().Be("Fizz");
     }
     
 }
 
 public class Output
 {
-    public string value;
+    public string output;
+    private int value;
 
     public Output(int value)
     {
+        this.value = value;
+        ToString(value);
+    }
+
+    void ToString(int value)
+    {
         if(value % 3 == 0)
         {
-            this.value = "Fizz";
+            this.output = "Fizz";
+            return;
         }
-        else
-        {
-            this.value = value.ToString();
-        }
+        
+        this.output = value.ToString();
     }
 }
