2bf252a54fc6a61c4248b7df18f31fae082db138
cmolina <claramolinadev@gmail.com>
2024-01-12 11:14:02 +0100

    feat: output class is now static

--- FizzBuzzTests/UnitTest1.cs
+++ FizzBuzzTests/UnitTest1.cs
@@ -9,34 +9,30 @@ public class Tests
     [Test]
     public void dsfghfdsgh()
     {
-        var sut = new Output();
         Output.FizzBuzz(1).Should().Be("1");
     }
     
     [Test]
     public void shgfdghdfg()
     {
-        var sut = new Output();
         Output.FizzBuzz(2).Should().Be("2");
     }
     
     [Test]
     public void sdfghdfghd()
     {
-        var sut = new Output();
         Output.FizzBuzz(3).Should().Be("Fizz");
     }
     
     [Test]
     public void hgjfghjfgh()
     {
-        var sut = new Output();
         Output.FizzBuzz(6).Should().Be("Fizz");
     }
     
 }
 
-public class Output
+public static class Output
 {
     public static string FizzBuzz(int value)
     {
