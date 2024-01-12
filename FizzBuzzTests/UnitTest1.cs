e335892fdaff0d80c826ffe92ad7e22f19cc31cc
cmolina <claramolinadev@gmail.com>
2024-01-12 11:29:03 +0100

    refactor: apply braces style

--- FizzBuzzTests/UnitTest1.cs
+++ FizzBuzzTests/UnitTest1.cs
@@ -49,20 +49,14 @@ public static class FizzBuzz
     public static string FizzBuzzOf(int value)
     {
         if(value % 3 == 0 && value%5 == 0)
-        {
             return "FizzBuzz!";
-        }
-        
+
         if(value % 3 == 0)
-        {
             return "Fizz";
-        }
-        
+
         if(value % 5 == 0)
-        {
             return "Buzz";
-        }
-        
+
         return value.ToString();
     }
 }
