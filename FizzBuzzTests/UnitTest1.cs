78f10acf5466149ee68cb6cc625f52ac017d477f
cmolina <claramolinadev@gmail.com>
2024-01-12 11:35:08 +0100

    feat: console implementation

--- FizzBuzzTests/UnitTest1.cs
+++ FizzBuzzTests/UnitTest1.cs
@@ -43,20 +43,3 @@ public class Tests
         FizzBuzzOf(15).Should().Be("FizzBuzz!");
     }
 }
-
-public static class FizzBuzz
-{
-    public static string FizzBuzzOf(int value)
-    {
-        if(value % 3 == 0 && value%5 == 0)
-            return "FizzBuzz!";
-
-        if(value % 3 == 0)
-            return "Fizz";
-
-        if(value % 5 == 0)
-            return "Buzz";
-
-        return value.ToString();
-    }
-}
