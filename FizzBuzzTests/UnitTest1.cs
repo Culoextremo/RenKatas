c4e16d55b6d7e02a35e3058634231d5937b9ece2
cmolina <claramolinadev@gmail.com>
2024-01-12 10:29:24 +0100

    feat: added fizzbuzz files

---
+++ FizzBuzzTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using FluentAssertions;
+using FluentAssertions.Execution;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup() { }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
