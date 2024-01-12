93d10861ea10a19dd5c1a70d3de6c65d00ae442f
cmolina <claramolinadev@gmail.com>
2024-01-12 11:12:39 +0100

    feat: fizzbuzz method static

--- FizzBuzzTests/UnitTest1.cs
+++ FizzBuzzTests/UnitTest1.cs
@@ -10,38 +10,35 @@ public class Tests
     public void dsfghfdsgh()
     {
         var sut = new Output();
-        sut.FizzBuzz(1).Should().Be("1");
+        Output.FizzBuzz(1).Should().Be("1");
     }
     
     [Test]
     public void shgfdghdfg()
     {
         var sut = new Output();
-        sut.FizzBuzz(2).Should().Be("2");
+        Output.FizzBuzz(2).Should().Be("2");
     }
     
     [Test]
     public void sdfghdfghd()
     {
         var sut = new Output();
-        sut.FizzBuzz(3).Should().Be("Fizz");
+        Output.FizzBuzz(3).Should().Be("Fizz");
     }
     
     [Test]
     public void hgjfghjfgh()
     {
         var sut = new Output();
-        sut.FizzBuzz(6).Should().Be("Fizz");
+        Output.FizzBuzz(6).Should().Be("Fizz");
     }
     
 }
 
 public class Output
 {
-    public string output;
-    private int value;
-    
-    public string FizzBuzz(int value)
+    public static string FizzBuzz(int value)
     {
         if(value % 3 == 0)
         {
